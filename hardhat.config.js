153b6ca9f2ff97fee88f424e83a9bbe77584a57b
Jian Kim <jian7061@gmail.com>
2022-06-20 17:44:14 +0900

    talk to contract using js

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,12 +3,120 @@ require("dotenv").config();
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(account.address);
-  }
+task("checks", "Check contract amounts", async () => {
+  const [deployer] = await hre.ethers.getSigners();
+  const contractAddress = "0xF3496Fabc4eBc4CB5eBf6b3B575C933Bf804C904";
+  const abi = [
+    {
+      inputs: [
+        {
+          internalType: "uint256",
+          name: "_targetAmount",
+          type: "uint256",
+        },
+      ],
+      stateMutability: "nonpayable",
+      type: "constructor",
+    },
+    {
+      inputs: [],
+      name: "accumulatedAmount",
+      outputs: [
+        {
+          internalType: "uint256",
+          name: "",
+          type: "uint256",
+        },
+      ],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [],
+      name: "deadline",
+      outputs: [
+        {
+          internalType: "uint256",
+          name: "",
+          type: "uint256",
+        },
+      ],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [
+        {
+          internalType: "address",
+          name: "",
+          type: "address",
+        },
+      ],
+      name: "donatorToAmount",
+      outputs: [
+        {
+          internalType: "uint256",
+          name: "",
+          type: "uint256",
+        },
+      ],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [],
+      name: "owner",
+      outputs: [
+        {
+          internalType: "address",
+          name: "",
+          type: "address",
+        },
+      ],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [],
+      name: "refund",
+      outputs: [],
+      stateMutability: "nonpayable",
+      type: "function",
+    },
+    {
+      inputs: [],
+      name: "targetAmount",
+      outputs: [
+        {
+          internalType: "uint256",
+          name: "",
+          type: "uint256",
+        },
+      ],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [],
+      name: "withdrawDonation",
+      outputs: [],
+      stateMutability: "nonpayable",
+      type: "function",
+    },
+    {
+      stateMutability: "payable",
+      type: "receive",
+    },
+  ];
+  const FundraisingContract = new ethers.Contract(
+    contractAddress,
+    abi,
+    deployer
+  );
+  console.log(
+    await FundraisingContract.targetAmount(),
+    await FundraisingContract.accumulatedAmount()
+  );
 });
 
 // You need to export an object to set up your config
